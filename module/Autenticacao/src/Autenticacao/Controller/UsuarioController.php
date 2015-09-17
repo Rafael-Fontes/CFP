ad2ca92db1638d28ec7b3af13972d5684328f817
Rafael Fontes <rafaelk-f@hotmail.com>
2015-09-17 17:59:46 -0300

    Modulo autenticacao
    Criado modulo autenticacao

---
+++ module/Autenticacao/src/Autenticacao/Controller/UsuarioController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Autenticacao\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class UsuarioController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+}
